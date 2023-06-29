a40b41e1ae72080016996c5e694289b6b271f084
Jasan Hernandez <jasan814@gmail.com>
2023-06-28 18:02:19 -0600

    change to prod server

--- server.py
+++ server.py
@@ -3,16 +3,13 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 from reformat import reformat
-
 from transcribe import transcribe_yt
 
 app = Flask(__name__)
 CORS(app)
 
-# Uncomment this if you want to use an access token to access the API, set the access token here
-# access_token = 'qs40oEyAy/!N=llMGxweDb?oun6brI2Y3eFLmP/kqMZ3xMJVHu1Ew!Uniz66j=dIdeELZ-peCbBwe5PUafYVFLVZBr3OK5BkvOLlPSsvRlN0D?lofkDJbx?AfR3ZVUSpKx7fP1qcqvpuaOEhY310sRxj5zKSqC-6!JjkeUTbu-GMEF-jYE!Lkoj2wD7hSRFD9fAXsMKBo4DEKmSn2/xspM-H8BwT5VYmsHXi2N85kGUm3j2hUU4ABr0p?87XoX6-'  # Set the access token
-
 audio_directory = 'audio'  # The directory where the audio files will be saved
+app.config['DEBUG'] = False  # ensure this is False for production environment
 
 @app.route('/', methods=['GET'])
 def index():
@@ -20,18 +17,15 @@ def index():
 
 @app.route('/api/transcribe/<video_id>', methods=['GET'])
 def transcribe(video_id):
-    # Uncomment this if you want to use an access token to access the API
-    # if 'Authorization' not in request.headers or request.headers['Authorization'] != f'Bearer {access_token}':
-    #     return jsonify({'error': 'Unauthorized'}), 401
-
     app.logger.info(f"Transcribing video with id: {video_id}")
-    result = transcribe_yt(f'https://www.youtube.com/watch?v={video_id}')
-    formatted = reformat(result['segments'])
-
-    return jsonify({'script': formatted})
-
-
+    try:
+        result = transcribe_yt(f'https://www.youtube.com/watch?v={video_id}')
+        formatted = reformat(result['segments'])
+        return jsonify({'script': formatted})
+    except Exception as e:
+        app.logger.error(f"Error transcribing video with id: {video_id}")
+        app.logger.error(e)
+        return jsonify({'error': 'Error processing request'}), 500
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    app.run(port=3001, debug=True)
+    logging.basicConfig(filename='app.log', level=logging.INFO)
